649ceaaed51f63729f1f326310d5214861b312ea
Ivan Gagis <igagis@gmail.com>
2017-08-18 18:21:12 +0300

    stuff

--- src/mikroxml/mikroxml.cpp
+++ src/mikroxml/mikroxml.cpp
@@ -3,3 +3,6 @@
 
 using namespace mikroxml;
 
+void Parser::feed(const utki::Buf<char> data) {
+
+}
--- src/mikroxml/mikroxml.hpp
+++ src/mikroxml/mikroxml.hpp
@@ -1,6 +1,29 @@
 #pragma once
 
+#include <string>
+
+#include <utki/Buf.hpp>
 
 namespace mikroxml{
-
+class Parser{
+	
+public:
+	enum class Error_e{
+		
+	};
+	
+	virtual void onElementStart(const utki::Buf<char> name) = 0;
+	
+	virtual void onElementEnd() = 0;
+	
+	virtual void onAttributeParsed(const utki::Buf<char> name, const utki::Buf<char> value) = 0;
+	
+	virtual void onStringParsed(const utki::Buf<char> str) = 0;
+	
+	virtual void onError(unsigned lineNumber, Error_e error) = 0;
+	
+	void feed(const utki::Buf<char> data);
+	
+	virtual ~Parser()noexcept{}
+};
 }
