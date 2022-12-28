2917c6d576a8aab3025fbd4cfde04a27b09fc52d
Ivan Gagis <igagis@gmail.com>
2022-12-28 17:34:32 +0200

    fix msvs build

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -61,23 +61,23 @@ msbuild /m ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=v143_R
 Push-Location
 cd ../tests/unit
 
-    ../msvs_solution/v142_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_md.xml; If(!$?){exit 1}
-    ../msvs_solution/v142_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_mt.xml; If(!$?){exit 1}
-    ../msvs_solution/v142_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x86_v142_release_md.xml; If(!$?){exit 1}
-    ../msvs_solution/v142_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x86_v142_release_mt.xml; If(!$?){exit 1}
-../msvs_solution/x64/v142_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x64_v142_debug_md.xml; If(!$?){exit 1}
-../msvs_solution/x64/v142_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x64_v142_debug_mt.xml; If(!$?){exit 1}
-../msvs_solution/x64/v142_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x64_v142_release_md.xml; If(!$?){exit 1}
-../msvs_solution/x64/v142_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x64_v142_release_mt.xml; If(!$?){exit 1}
+    ../../msvs_solution/v142_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_md.xml; If(!$?){exit 1}
+    ../../msvs_solution/v142_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x86_v142_debug_mt.xml; If(!$?){exit 1}
+    ../../msvs_solution/v142_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x86_v142_release_md.xml; If(!$?){exit 1}
+    ../../msvs_solution/v142_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x86_v142_release_mt.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v142_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x64_v142_debug_md.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v142_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x64_v142_debug_mt.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v142_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x64_v142_release_md.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v142_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x64_v142_release_mt.xml; If(!$?){exit 1}
 
-    ../msvs_solution/v143_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x86_v143_debug_md.xml; If(!$?){exit 1}
-    ../msvs_solution/v143_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x86_v143_debug_mt.xml; If(!$?){exit 1}
-    ../msvs_solution/v143_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x86_v143_release_md.xml; If(!$?){exit 1}
-    ../msvs_solution/v143_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x86_v143_release_mt.xml; If(!$?){exit 1}
-../msvs_solution/x64/v143_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x64_v143_debug_md.xml; If(!$?){exit 1}
-../msvs_solution/x64/v143_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x64_v143_debug_mt.xml; If(!$?){exit 1}
-../msvs_solution/x64/v143_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x64_v143_release_md.xml; If(!$?){exit 1}
-../msvs_solution/x64/v143_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x64_v143_release_mt.xml; If(!$?){exit 1}
+    ../../msvs_solution/v143_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x86_v143_debug_md.xml; If(!$?){exit 1}
+    ../../msvs_solution/v143_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x86_v143_debug_mt.xml; If(!$?){exit 1}
+    ../../msvs_solution/v143_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x86_v143_release_md.xml; If(!$?){exit 1}
+    ../../msvs_solution/v143_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x86_v143_release_mt.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v143_Debug_MD/tests.exe       --jobs=2 --junit-out=junit_x64_v143_debug_md.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v143_Debug_MT/tests.exe       --jobs=2 --junit-out=junit_x64_v143_debug_mt.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v143_Release_MD/tests.exe     --jobs=2 --junit-out=junit_x64_v143_release_md.xml; If(!$?){exit 1}
+../../msvs_solution/x64/v143_Release_MT/tests.exe     --jobs=2 --junit-out=junit_x64_v143_release_mt.xml; If(!$?){exit 1}
 
 Pop-Location
 
